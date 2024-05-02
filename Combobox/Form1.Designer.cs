b468c319f9f9cb08ecf4f6d2cf2a351c4e090b26
Al3ksemduble <ah097786@gmail.com>
2024-05-02 15:01:57 -0300

    Adicionar arquivos de projeto.

---
+++ Combobox/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Combobox
+{
+    partial class FrmComboBox
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboCidades = new System.Windows.Forms.ComboBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnVerif = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblInstrucao1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cboCidades
+            // 
+            this.cboCidades.FormattingEnabled = true;
+            this.cboCidades.Items.AddRange(new object[] {
+            "Sidney",
+            "Paris",
+            "Roma",
+            "São Paulo"});
+            this.cboCidades.Location = new System.Drawing.Point(363, 108);
+            this.cboCidades.Name = "cboCidades";
+            this.cboCidades.Size = new System.Drawing.Size(121, 21);
+            this.cboCidades.TabIndex = 0;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblTitulo.Location = new System.Drawing.Point(364, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(154, 31);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Combo Box";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblResult.Location = new System.Drawing.Point(15, 42);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(72, 17);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "Resultado";
+            // 
+            // btnVerif
+            // 
+            this.btnVerif.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnVerif.Location = new System.Drawing.Point(366, 245);
+            this.btnVerif.Name = "btnVerif";
+            this.btnVerif.Size = new System.Drawing.Size(118, 34);
+            this.btnVerif.TabIndex = 3;
+            this.btnVerif.Text = "Verificar";
+            this.btnVerif.UseVisualStyleBackColor = true;
+            this.btnVerif.Click += new System.EventHandler(this.btnVerif_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblResult);
+            this.groupBox1.Location = new System.Drawing.Point(40, 285);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(444, 92);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnSair.Location = new System.Drawing.Point(21, 404);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(118, 34);
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lblInstrucao1
+            // 
+            this.lblInstrucao1.AutoSize = true;
+            this.lblInstrucao1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblInstrucao1.Location = new System.Drawing.Point(210, 109);
+            this.lblInstrucao1.Name = "lblInstrucao1";
+            this.lblInstrucao1.Size = new System.Drawing.Size(147, 17);
+            this.lblInstrucao1.TabIndex = 6;
+            this.lblInstrucao1.Text = "Selecione uma cidade";
+            // 
+            // FrmComboBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 450);
+            this.Controls.Add(this.lblInstrucao1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnVerif);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.cboCidades);
+            this.Name = "FrmComboBox";
+            this.Text = "Combobox";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboCidades;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnVerif;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblInstrucao1;
+    }
+}
+
---
+++ Combobox/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Combobox
+{
+    public partial class FrmComboBox : Form
+    {
+        public FrmComboBox()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerif_Click(object sender, EventArgs e)
+        {
+            if (cboCidades.SelectedItem.ToString() == "Sidney")
+            {
+                lblResult.Text = "Você ama a cidade das praias!";
+            }
+            else if (cboCidades.SelectedItem.ToString() == "Paris")
+            {
+                lblResult.Text = "Você ama a cidade da luz!";
+            }
+            else if (cboCidades.SelectedItem.ToString() == "Roma")
+            {
+                lblResult.Text = "Você ama a cidade eterna!";
+            }
+            else if (cboCidades.SelectedItem.ToString() == "São Paulo")
+            {
+                lblResult.Text = "Você tem bom gosto para viagens";
+            }
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
